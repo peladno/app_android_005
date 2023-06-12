5c2d8fa451f4c998ae7c3c24d3cd2c550408d600
Javier <j.perezurrutia@gmail.com>
2023-06-11 21:03:01 -0400

    done

---
+++ app/src/main/java/com/example/ejercicio7_modulo4_android/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ejercicio7_modulo4_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "¡Bienvenido!", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
